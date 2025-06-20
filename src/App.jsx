547a0bae319e8cac961c67372c781407852be252
Yuvraj <yuvrajbansal30dec@gmail.com>
2025-06-20 23:19:00 +0530

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Wishlist from './components/Wishlist';
 import Cart from './components/Cart';
@@ -9,20 +9,7 @@ import Header from './components/Header.jsx';
 import TrackOrder from './components/TrackOrder';
 import FooterSection from './components/FooterSection';
 
-const App = () => {
-  const [showSignIn, setShowSignIn] = useState(false);
-  const [showSignUp, setShowSignUp] = useState(false);
-
-  const handleSwitchToSignUp = () => {
-    setShowSignIn(false);
-    setShowSignUp(true);
-  };
-
-  const handleSwitchToSignIn = () => {
-    setShowSignUp(false);
-    setShowSignIn(true);
-  };
-
+function App(){
   return (
     <Router>
       
